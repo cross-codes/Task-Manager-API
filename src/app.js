091013d8aadf8037a4e4d9f64f03dcea9bc7166b
Akshaj Rao <akshajrao2004@gmail.com>
2023-08-20 12:30:58 +0530

    Move maintenance mode to index.js

--- src/app.js
+++ src/app.js
@@ -4,16 +4,6 @@ import taskRouter from "./routers/task.js";
 import userRouter from "./routers/user.js";
 
 const app = express();
-const maintenanceMode = false;
-
-app.use((req, res, next) => {
-  if (!maintenanceMode) {
-    next();
-  } else {
-    res.status(503);
-    res.send("The server is temporarily down, please try again later");
-  }
-});
 
 app.use(express.json());
 app.use(userRouter);
